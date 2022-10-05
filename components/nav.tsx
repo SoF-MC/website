a46e5e766889ad348cc54610a96f4d00eefa008c
oplotDev <oplotDev@gmail.com>
2022-10-05 15:42:15 +0300

    Nav text fix

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,7 +4,7 @@ export default class Nav extends React.Component {
     render() {
         return (
             <nav className="fixed w-full h-16 font-montserrat border-b-2 transition items-center z-50 select-none bg-zinc-900/80 backdrop-blur-sm border-zinc-900/50 flex">
-                <p className='ml-32 pixel:ml-8 text-white'>
+                <p className='ml-32 pixel:ml-8 text-white flex-none'>
                     #SoF Mods
                 </p>
                 <div className='ml-[640px] pixel:ml-16'>
