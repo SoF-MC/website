50f803d7bd6afb5af1e524c42202540626c9f8fb
oplotDev <oplotDev@gmail.com>
2022-10-05 17:08:18 +0300

    Question of Question

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,7 @@ import arm from '../public/arm.png'
 import heart from '../public/heart.png'
 import hammer_and_pick from '../public/hammer-and-pick.png'
 import event from '../public/event.png'
+import question_mark from '../public/question_mark.png'
 
 export default class Index extends React.Component {
     render() {
@@ -51,6 +52,22 @@ export default class Index extends React.Component {
                             </div>
                             <h2 className="text-3xl font-extrabold font-manrope tracking-tight mb-6 text-center text-purple-500">Ивенты</h2>
                             <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Игроки сервера могут проводить собственные ивенты! Если вы хотите свои предметы для ивента попросите админов!</p>
+                        </div>
+                    </div>
+                    <div className="container px-6 mx-auto my-12 grid gap-12">
+                        <div className="py-12 bg-slate-800/10 rounded-[48px] px-6">
+                            <div className="grid place-items-center mb-3">
+                                <Image src={question_mark} width={64} height={64} />
+                            </div>
+                            <h2 className="text-3xl font-extrabold font-manrope tracking-tight mb-6 text-center text-slate-500">Остались вопросы?</h2>
+                            <div className='grid justify-items-center'>
+                                <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Задавай их смело в нашем Discord сервере!</p>
+                                <button className='-mt-6 py-3 px-5 bg-zinc-500/10 hover:bg-zinc-400/10 rounded-2xl text-white text-manrope'>
+                                    <a href='https://discord.gg/jZudXQM'>
+                                        Discord сервер
+                                    </a>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
