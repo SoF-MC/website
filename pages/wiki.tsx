7350f6f26841a6fd80d029008f8b32906d6a23e4
oplotDev <oplotDev@gmail.com>
2022-10-05 16:55:00 +0300

    Wiki init

---
+++ pages/wiki.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import hammer_and_pick from '../public/hammer-and-pick.png'
+
+export class Wiki {
+	render() {
+		return (
+			<div className='h-screen grid place-items-center'>
+				<div className='flex items-center'>
+					<Image src={hammer_and_pick} width={64} height={64} />
+					<p className='text-white font-montserrat mx-6 text-2xl text-center'>
+						Вики в разработке
+					</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default new Wiki().render
