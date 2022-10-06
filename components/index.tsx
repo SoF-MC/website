328aac12b5fa06f49e3be6b9dae36eba4e35829f
oplotDev <oplotDev@gmail.com>
2022-10-06 20:21:51 +0300

    Donate

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,8 @@ import arm from '../public/arm.png'
 import heart from '../public/heart.png'
 import hammer_and_pick from '../public/hammer-and-pick.png'
 import event from '../public/event.png'
+import oplotDev from '../public/oplotDev.png'
+import Vladirus from '../public/Vladirus.png'
 
 export default class Index extends React.Component {
     render() {
@@ -53,16 +55,32 @@ export default class Index extends React.Component {
                             <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Игроки сервера могут проводить собственные ивенты! Если вы хотите свои предметы для ивента попросите админов!</p>
                         </div>
                     </div>
-                    <div className="container px-6 mx-auto my-12 grid gap-12">
-                        <div className="py-12 bg-slate-800/10 rounded-[48px] px-6">
-                            <h2 className="text-3xl font-extrabold font-manrope tracking-tight mb-6 text-center text-slate-500">Остались вопросы?</h2>
+                    <div className='font-manrope text-3xl justify-items-center w-full text-center'>
+                        Хотите задонатить?
+                    </div>
+                    <div className="container px-6 mx-auto my-12 grid gap-12 lg:grid-cols-2">
+                        <div className="py-12 bg-fuchsia-900/10 rounded-[48px] px-6">
+                            <div className="grid place-items-center mb-3">
+                                <Image src={Vladirus} width={64} height={64} className='rounded-[24px]'/>
+                            </div>
+                            <h2 className="text-3xl font-extrabold font-manrope tracking-tight mb-6 text-center text-fuchsia-500">Vladirus</h2>
                             <div className='grid justify-items-center'>
-                                <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Задавай их смело в нашем Discord сервере!</p>
-                                <button className='-mt-6 py-3 px-5 bg-zinc-500/10 hover:bg-zinc-400/10 rounded-2xl text-white text-manrope'>
-                                    <a href='https://discord.gg/jZudXQM'>
-                                        Discord сервер
-                                    </a>
-                                </button>
+                            <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Создатель оригинального #SoF'a, ютубер, стример</p>
+                                <a href='https://www.donationalerts.com/r/vladirus' className='-mt-6 py-3 px-5 bg-zinc-500/10 hover:bg-zinc-400/10 rounded-2xl text-white text-manrope'>
+                                    DonationAlerts
+                                </a>
+                            </div>
+                        </div>
+                        <div className="py-12 bg-zinc-700/10 rounded-[48px] px-6">
+                            <div className="grid place-items-center mb-3">
+                                <Image src={oplotDev} width={64} height={64} className='rounded-[24px]'/>
+                            </div>
+                            <h2 className="text-3xl font-extrabold font-manrope tracking-tight mb-6 text-center text-zinc-500">MeiNanziiii (oplotDev)</h2>
+                            <div className='grid justify-items-center'>
+                                <p className="leading-relaxed max-w-lg mx-auto mb-8 text-center">Создатель и идея #SoF Mods, создатель сайта</p>
+                                <a href='https://www.buymeacoffee.com/meinanziiii' className='-mt-6 py-3 px-5 bg-zinc-500/10 hover:bg-zinc-400/10 rounded-2xl text-white text-manrope'>
+                                    Buy me a coffee
+                                </a>
                             </div>
                         </div>
                     </div>
