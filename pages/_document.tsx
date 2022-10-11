d6918b675ecd6a4982c364b5b26bb58b9dad2abf
oplotDev <oplotDev@gmail.com>
2022-10-11 17:03:43 +0300

    Logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="icon" href="/logo.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Manrope:wght@800&family=Montserrat&display=swap" rel="stylesheet" />
