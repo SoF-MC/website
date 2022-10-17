9f0f549456a33908325ef8303a7ae5c16bd07789
Vadym <me@djoh.xyz>
2022-10-17 14:48:35 +0000

    😠

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -1,6 +1,6 @@
-import { setCookie } from 'cookies-next';
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
+import { setCookie } from "cookies-next";
 
 export const setUser = (req: NextApiRequest, res: NextApiResponse, token: string) => {
-    setCookie("user", token, { req, res, maxAge: 1000 * 60 * 60 * 24 })
-}
+    return setCookie("user", token, { req, res, maxAge: 1000 * 60 * 60 * 24 });
+};
