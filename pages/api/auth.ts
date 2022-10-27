54aab7f78927ca2012d48b49f4a4825504971589
Vadym <me@djoh.xyz>
2022-10-27 19:52:37 +0000

    fix

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { setUser } from "../../utils/cookies";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (!req.query.auth) return res.status(403).send();
+    if (!req.query.auth) return res.status(403).send("");
     setUser(req, res, req.query.auth as string);
     return res.redirect("/");
 };
